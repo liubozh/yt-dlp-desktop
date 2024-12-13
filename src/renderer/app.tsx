8fc348e0711e26cafacf1c80f53281d8c92dd5ba
liubozh <bo.liu@hey.com>
2024-12-13 22:38:53 +0800

    add shadcn ui

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -1,8 +1,10 @@
+import Layout from './layout';
+import { AppSidebar } from './components/app-sidebar';
+
 export default function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">💖 Hello World!</h1>
-      <p>Welcome to your Electron application.</p>
-    </div>
+    <Layout>
+      <AppSidebar />
+    </Layout>
   );
 }
