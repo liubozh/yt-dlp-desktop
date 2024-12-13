c3aefc81fc9951a2164bf1fce3a680a519532fa6
liubozh <bo.liu@hey.com>
2024-12-13 17:58:20 +0800

    add tailwindcss

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div>
-      <h1>💖 Hello World!</h1>
+      <h1 className="text-3xl font-bold underline">💖 Hello World!</h1>
       <p>Welcome to your Electron application.</p>
     </div>
   );
