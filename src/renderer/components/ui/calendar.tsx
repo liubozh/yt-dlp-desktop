45d2d7de99ef270951f1a827c13e736c15071356
liubozh <bo.liu@hey.com>
2024-12-16 18:57:57 +0800

    fix calendar.tsx

--- src/renderer/components/ui/calendar.tsx
+++ src/renderer/components/ui/calendar.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ChevronLeft, ChevronRight } from 'lucide-react';
+import { ChevronLeftIcon, ChevronRightIcon } from 'lucide-react';
 import { DayPicker } from 'react-day-picker';
 
 import { cn } from '../../lib/utils';
@@ -58,8 +58,11 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        Chevron: ({ orientation }) => {
+          const Icon =
+            orientation === 'left' ? ChevronLeftIcon : ChevronRightIcon;
+          return <Icon className="h-4 w-4" />;
+        },
       }}
       {...props}
     />
